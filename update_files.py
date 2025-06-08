4906a1df8ac4faea252b69c92d5a4e4a30bc8c91
SanghunBruceHam <123874866+SanghunBruceHam@users.noreply.github.com>
2025-06-08 13:54:50 +0900

    Update update_files.py

--- update_files.py
+++ update_files.py
@@ -5,6 +5,8 @@ robots_txt = """User-agent: *
 Allow: /
 
 Sitemap: https://www.mahalohana-bruce.com/sitemap.xml
+
+#DaumWebMasterTool:160f060c046ce0629c1cd68a964c2d8f836de4219c3d362711b8bb43b5b92e42:NnJbOlYdct0Jzm/G1OzHmA==
 """
 
 with open("robots.txt", "w", encoding="utf-8") as f:
@@ -12,7 +14,6 @@ with open("robots.txt", "w", encoding="utf-8") as f:
 
 # 2. ads.txt
 ads_txt = """google.com, pub-5508768187151867, DIRECT, f08c47fec0942fa0
-google.com, pub-1111111111111111, DIRECT, f08c47fec0942fa0
 # Last updated: {}
 """.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC'))
 
